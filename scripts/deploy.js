305640904e92f1ec11bddda9b1020df6e2e3890e
Aashish <aashisharora0786@gmail.com>
2023-08-24 13:50:38 +0530

    added env variable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,21 +17,5 @@ async function main() {
   return myNFT.address
 }
 main()
-// module.exports = main;
 
-// async function main() {
-//   const MyNFT = await ethers.getContractFactory("MyNFT")
-
-//   // Start deployment, returning a promise that resolves to a contract object
-//   const myNFT = await MyNFT.deploy()
-//   await myNFT.deployed()
-//   console.log("Contract deployed to address:", myNFT.address)
-// }
-
-
-  // .then(() => process.exit(0))
-  // .catch((error) => {
-  //   console.error(error)
-  //   process.exit(1)
-  // })
 
--- server.js
+++ server.js
@@ -2,6 +2,68 @@ require("dotenv").config();
 const express = require('express');
 const { ethers } = require('ethers');
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const multer = require('multer');
+const axios = require('axios');
+
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const PORT = 3000;
+
+// Migrating the two NFT Api's into one
+app.post('/uploadAsset', upload.single('file'), async (req, res) => {
+    const bearerToken = req.headers.authorization; // "Authorization: Bearer TOKEN_VALUE"
+
+    try {
+        if (!req.query.json) {
+            return res.status(400).send({ error: 'JSON parameter is missing' });
+        }
+        const clientJson = JSON.parse(req.query.json); // parse the JSON string from the URL query parameter
+        // Send the binary file to the first API
+        const firstApiResponse = await axios.post('https://api.nft.storage/upload', req.file.buffer, {
+            headers: {
+                'Authorization': bearerToken,
+                'Content-Type': req.file.mimetype
+            }
+        });
+
+        const extractedValue = firstApiResponse.data.value.cid; // Adjust to match actual field name
+        const extractedlink = "ipfs://"+extractedValue
+
+        // Modify the parsed JSON with the extracted value
+        clientJson.ipfsImageLink = extractedlink; 
+        // Send the modified JSON payload to the second API
+        const secondApiResponse = await axios.post('https://api.nft.storage/upload', clientJson, {
+            headers: {
+                'Authorization': bearerToken,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        ApiResponse = secondApiResponse.data.value.cid;
+        const extractedlink2 = "ipfs://"+ApiResponse
+        res.send({"cid":extractedlink2})
+
+    } catch (error) {
+        res.status(500).send({ error: 'There was an error chaining the APIs.' });
+    }
+});
+
+const DB_NAME= process.env.DB_NAME
+const DB_HOST=process.env.DB_HOST
+const DB_PORT=process.env.DB_PORT
+const DB_USER=process.env.DB_USER
+const DB_PASSWORD= process.env.DB_PASSWORD
+
+//database credentials
+const { Sequelize, DataTypes } = require('sequelize');
+// Configure Sequelize
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port:DB_PORT,
+    dialect: 'mysql' // e.g., 'mysql', 'postgres', etc.
+});
 
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
@@ -12,13 +74,13 @@ const contractPath = path.join(__dirname, '../create-nft/artifacts/contracts/MyN
 const contract = require(contractPath);
 const web3 = createAlchemyWeb3(API_URL);
 
-const app = express();
 // const deploy = require('./scripts/deploy');  // Path to your Hardhat script
 app.use(express.json()); // For parsing application/json
 
 const { exec } = require('child_process');
 
-app.post('/deployAndMintNFT', (req, res) => {
+//Creating NFT and minting the same
+app.post('/MintNFT', (req, res) => {
     const tokenURI = req.body.tokenURI;
 
     if (!tokenURI) {
@@ -93,13 +155,50 @@ app.post('/deployAndMintNFT', (req, res) => {
     });
 });
 
+// Define the model based on your Python model
+const PersonalInfo = sequelize.define('Identity', {
+    user_id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+    },
+    token_id: DataTypes.STRING(1000)
+}, {
+    tableName: 'identity',  
+    timestamps: false
+});
+
+// Update token API
+app.post('/updateToken', async (req, res) => {
+    try {
+        const { user_id, token } = req.body;
+
+        if (!user_id || !token) {
+            return res.status(400).json({ message: 'userID and token are required' });
+        }
+
+        const user = await PersonalInfo.findOne({ where: { user_id: user_id } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.token = token;
+        await user.save();
+
+        res.json({ message: 'Token updated successfully!' });
+
+    } catch (error) {
+        console.error('Error updating token:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal server error.');
 });
 
-const PORT = 3002;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
